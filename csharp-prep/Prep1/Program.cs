48cdac0f45b552e2670e90c19ad32f307b0d1f93
roliveira89 <raphaeloliveira89@gmail.com>
2023-04-23 22:34:20 +0100

    Added comments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Asks user's first name
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
-
+        // Asks user's last name
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
-
+        // Print user's name to the terminal
         Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
