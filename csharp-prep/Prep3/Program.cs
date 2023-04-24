d03940a99404b9dc910a04d28b9d38e08fd5f78f
roliveira89 <raphaeloliveira89@gmail.com>
2023-04-24 23:50:12 +0100

    Finished prep 3 with stretch

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // User provides the magic number
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        // User guesses the number
+        // Console.Write("What is your guess? ");
+        // int guess = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Let's play a game.");
+        Console.WriteLine("Try to guess a number from 1 to 100.");
+
+        do
+        {
+            PlayGame();
+            Console.WriteLine("Would you play to play again? Y or N");
+        } while (Console.ReadLine().ToLower() == "y");
+    }
+
+    public static void PlayGame()
+    {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        int guesses = 0;
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                guesses++;
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                guesses++;
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                guesses++;
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You did it in {guesses} attempts.");
+            }
+        }                    
     }
 }
